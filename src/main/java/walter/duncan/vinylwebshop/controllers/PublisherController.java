ce7857a7405ccdceb4ecc648c6cd2a835c0ee934
Duncan Walter <duncan1997@hotmail.nl>
2025-11-26 16:33:56 +0100

    Implemented PublisherController in the same way as GenreController

---
+++ src/main/java/walter/duncan/vinylwebshop/controllers/PublisherController.java
@@ -0,0 +1,58 @@
+package walter.duncan.vinylwebshop.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import walter.duncan.vinylwebshop.entities.PublisherEntity;
+import walter.duncan.vinylwebshop.services.PublisherService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/publishers")
+public class PublisherController {
+    private final PublisherService publisherService;
+
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<PublisherEntity> getPublisherById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.publisherService.findPublisherById(id));
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<PublisherEntity>> getPublishers() {
+        return ResponseEntity.ok(this.publisherService.findAllPublishers());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<PublisherEntity> createPublisher(@RequestBody PublisherEntity publisher) {
+        var publisherEntity = this.publisherService.createPublisher(publisher);
+        var location = URI.create(String.format("http://localhost:8080/publisher/%s", publisherEntity.getId()));
+
+        return ResponseEntity.created(location).body(publisherEntity);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<PublisherEntity> updatePublisher(@PathVariable Long id, @RequestBody PublisherEntity publisher) {
+        var updatePublisher = this.publisherService.updatePublisher(id, publisher);
+        var location = URI.create(String.format("http://localhost:8080/publisher/%s", id));
+
+        return ResponseEntity.ok().location(location).body(updatePublisher);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deletePublisher(@PathVariable Long id) {
+        this.publisherService.deletePublisher(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
