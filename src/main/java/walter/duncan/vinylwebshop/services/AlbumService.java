18aa051e48273fec63fb87dbdd94b65c852cd689
Duncan Walter <duncan1997@hotmail.nl>
2025-12-10 14:34:23 +0100

    Added Album, Artist and Stock services - persistence methods need more work

---
+++ src/main/java/walter/duncan/vinylwebshop/services/AlbumService.java
@@ -0,0 +1,51 @@
+package walter.duncan.vinylwebshop.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import walter.duncan.vinylwebshop.dtos.album.AlbumRequestDto;
+import walter.duncan.vinylwebshop.dtos.album.AlbumResponseDto;
+import walter.duncan.vinylwebshop.entities.AlbumEntity;
+import walter.duncan.vinylwebshop.mappers.AlbumDtoMapper;
+import walter.duncan.vinylwebshop.repositories.AlbumRepository;
+
+import java.util.List;
+
+@Service
+public class AlbumService extends BaseService<AlbumEntity, Long> {
+    private final AlbumDtoMapper albumDtoMapper;
+
+    protected AlbumService(AlbumRepository albumRepository, AlbumDtoMapper albumDtoMapper) {
+        super(albumRepository);
+        this.albumDtoMapper = albumDtoMapper;
+    }
+
+    public List<AlbumResponseDto> findAllAlbums() {
+        return this.albumDtoMapper.toDto(this.repository.findAll());
+    }
+
+    public AlbumResponseDto findAlbumById(Long id) {
+        return this.albumDtoMapper.toDto(this.getExistingById(id));
+    }
+
+    public AlbumResponseDto createAlbum(AlbumRequestDto albumRequestDto) {
+        var albumEntity = this.albumDtoMapper.toEntity(albumRequestDto);
+
+        return this.albumDtoMapper.toDto(this.repository.save(albumEntity));
+    }
+
+    @Transactional
+    public AlbumResponseDto updateAlbum(Long id, AlbumRequestDto albumRequestDto) {
+        var persistedEntity = this.getExistingById(id);
+        persistedEntity.setTitle("placeholder");
+        persistedEntity.setReleaseYear(9999);
+
+        return this.albumDtoMapper.toDto(this.repository.save(persistedEntity));
+    }
+
+    @Transactional
+    public void deleteAlbum(Long id) {
+        this.ensureExistsById(id);
+        this.repository.deleteById(id);
+    }
+}
---
+++ src/main/java/walter/duncan/vinylwebshop/services/StockService.java
@@ -0,0 +1,51 @@
+package walter.duncan.vinylwebshop.services;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import walter.duncan.vinylwebshop.dtos.stock.StockRequestDto;
+import walter.duncan.vinylwebshop.dtos.stock.StockResponseDto;
+import walter.duncan.vinylwebshop.entities.StockEntity;
+import walter.duncan.vinylwebshop.mappers.StockDtoMapper;
+import walter.duncan.vinylwebshop.repositories.StockRepository;
+
+import java.util.List;
+
+@Component
+public class StockService extends BaseService<StockEntity, Long> {
+    private final StockDtoMapper stockDtoMapper;
+
+    protected StockService(StockRepository stockRepository, StockDtoMapper stockDtoMapper) {
+        super(stockRepository);
+        this.stockDtoMapper = stockDtoMapper;
+    }
+
+    public List<StockResponseDto> findAllStocks() {
+        return this.stockDtoMapper.toDto(this.repository.findAll());
+    }
+
+    public StockResponseDto findStockById(Long id) {
+        return this.stockDtoMapper.toDto(this.getExistingById(id));
+    }
+
+    public StockResponseDto createStock(StockRequestDto stockRequestDto) {
+        var stockEntity = this.stockDtoMapper.toEntity(stockRequestDto);
+
+        return this.stockDtoMapper.toDto(this.repository.save(stockEntity));
+    }
+
+    @Transactional
+    public StockResponseDto updateStock(Long id, StockRequestDto stockRequestDto) {
+        var persistedEntity = getExistingById(id);
+        persistedEntity.setCondition("placeholder");
+        persistedEntity.setPrice(9999);
+
+        return this.stockDtoMapper.toDto(this.repository.save(persistedEntity));
+    }
+
+    @Transactional
+    public void deleteStock(Long id) {
+        this.ensureExistsById(id);
+        this.repository.deleteById(id);
+    }
+}
